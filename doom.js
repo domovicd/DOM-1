e623e171086e1ab0e3fe22035903699e6b2b3945
domovicd <domovic84@gmail.com>
2017-04-27 10:17:15 +0300

    Add files via upload

---
+++ doom.js
@@ -0,0 +1,18 @@
+/**var as=document.getElementById("prvi").innerHTML;
+document.getElementById("drugi").innerHTML=as;**/
+
+function fan() {
+    document.getElementById("silka").src="truba.jpg";
+}
+function fun() {
+    document.getElementById("silka").src="trubica.jpg";
+}
+function promjenaboje() {
+    document.getElementById("prvi").style.backgroundColor="blue";
+}
+function sakrijsliku() {
+    document.getElementById("divcic").style.display="none";
+}
+function prikazisliku() {
+    document.getElementById("divcic").style.display="block";
+}
